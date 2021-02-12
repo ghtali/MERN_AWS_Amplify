c07d353890605de5f295053156ef8fffb42fa58a
A.G <ght.ali@hotmail.com>
2021-02-12 15:48:33 +0100

    Implemenetation of Persons list

--- src/Components/App.js
+++ src/Components/App.js
@@ -49,15 +49,15 @@ class App extends React.Component {
       <div className="container-fluid">
         <div className="row">
           <nav>
-            <div class="nav-wrapper">
-              <a href="#" class="brand-logo">Logo</a>
-              <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <div className="nav-wrapper grey darken-2">
+              <a href="/" className="brand-logo">MERN Amplify</a>
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><a href="sass.html">Sass</a></li>
                 <li><a href="badges.html">Components</a></li>
                 <li><a href="collapsible.html">JavaScript</a></li>
               </ul>
             </div>
-          </nav>;
+          </nav>
 
         </div>
         <div className="row">
@@ -67,7 +67,7 @@ class App extends React.Component {
               updateCurrentPerson={this.updateCurrentPerson}
             />
           </div>
-          <div className="col s9"><PersonSingle /></div>
+          <div className="col s9"><PersonSingle person={this.state.currentPerson}/></div>
         </div>
         <div className="row">
           <div className="col s12"><PersonForm /></div>
--- src/Components/Person/PersonSingle.js
+++ src/Components/Person/PersonSingle.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
 //created stateless function component
-const PersonSingle = () => {
+const PersonSingle = (props) => {
   return (
-    <div>
-      PersonSingle{' '}
+      <div className="row">
+    <div className="col s12">
+      <div className="card">
+        <div className="card-image">
+          <img src="employee.png" alt=""/>
+          <span className="card-title">
+          {props.person.firstName} {props.person.lastName}
+          </span>
+        </div>
+        <div className="card-content">
+          <p>
+            Phone: {props.person.phone} - Email: {props.person.email}
+            Techniques: {props.person.techniques} - Strength: {props.person.strength}
+            Speed: {props.person.speed} - Ability: {props.person.ability}
+          </p>
+        </div>
+        <div className="card-action">
+          Team: {props.person.team}
+        </div>
+      </div>
     </div>
+  </div>
   );
 };
 export default PersonSingle;
