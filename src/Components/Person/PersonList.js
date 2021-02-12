e53534c2f3e13b4917801d6d59d9cab2a7fa9aa9
A.G <ght.ali@hotmail.com>
2021-02-12 10:48:20 +0100

    adding axios, added updating person functionality

--- src/Components/Person/PersonList.js
+++ src/Components/Person/PersonList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 //created stateless function component
+//Listing our persons component
+//to see list of all persons
 const PersonList = () => {
   return (
     <div>
-      PersonSingle{' '}
+      PersonList
     </div>
   );
 };
